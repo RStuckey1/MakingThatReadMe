9aa8c15beada2b3b9a7f725eccdea18d0f170d11
Rick S <stuckey85@gmail.com>
2024-08-12 20:08:58 -0500

    final code

---
+++ Develop/index.js
@@ -0,0 +1,77 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project.',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List the features of the project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them.'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'If you have any questions, reach out to me at my profile.'
+    }
+];
+
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('README.md created!')
+    );
+
+}
+
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            writeToFile('README.md', answers);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,89 @@
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license === 'Apache 2.0') {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === 'GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else {
+    return 'No License';
+  }
+};
+
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    default:
+      return '';
+    case 'None':
+      return 'No License';
+  }
+};
+
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return;
+  }
+  if (license === 'MIT') {
+    return 'MIT License';
+  }
+  if (license === 'Apache 2.0') {
+    return 'Apache 2.0 License';
+  }
+  if (license === 'GPL 3.0') {
+    return 'GPL 3.0 License';
+  }
+
+};
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+
+## Badges
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
+${data.license}
+
+## Features
+${data.features}
+
+## How to Contribute
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## GitHub/Questions
+${data.questions}`
+};
+
+export default generateMarkdown;
